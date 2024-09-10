e6da5954c8dba4173cdd63dc4eae23d912fa19cf
Stephen Saunders <saunders.stephen@gmail.com>
2024-09-10 15:32:33 -0700

    Add unit tests for JSONFileDatabase read and write operations

--- src/db.rs
+++ src/db.rs
@@ -10,3 +10,115 @@ trait Database {
 struct JSONFileDatabase {
     pub file_path: String,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod database {
+        use std::collections::HashMap;
+        use std::hash::Hash;
+        use std::io::Write;
+
+        use crate::models::{Epic, Status, Story};
+
+        use super::JSONFileDatabase;
+
+        use super::*;
+
+        #[test]
+        fn read_db_should_fail_with_invalid_path() {
+            let db = JSONFileDatabase {
+                file_path: "INVALID PATH".to_owned(),
+            };
+            assert_eq!(db.read_db().is_err(), true);
+        }
+
+        #[test]
+        fn read_db_should_fail_with_invalid_json() {
+            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+
+            let invalid_json_contents = r#"{ "last_item_id": 0 epics: {} stories {} }"#;
+            write!(tmpfile, "{}", invalid_json_contents).unwrap();
+
+            let db = JSONFileDatabase {
+                file_path: tmpfile
+                    .path()
+                    .to_str()
+                    .expect("failed to convert tmpfile to str")
+                    .to_string(),
+            };
+
+            let result = db.read_db();
+            assert_eq!(result.is_err(), true)
+        }
+
+        #[test]
+        fn read_db_should_parse_json_file() {
+            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+
+            let valid_json_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {}}"#;
+            write!(tmpfile, "{}", valid_json_contents).unwrap();
+
+            let db = JSONFileDatabase {
+                file_path: tmpfile
+                    .path()
+                    .to_str()
+                    .expect("failed to convert tmpfile path to str")
+                    .to_string(),
+            };
+
+            let result = db.read_db();
+
+            assert_eq!(result.is_ok(), true);
+        }
+
+        #[test]
+        fn write_db_should_work() {
+            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+
+            let valid_json_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {}}"#;
+            write!(tmpfile, "{}", valid_json_contents).unwrap();
+
+            let db = JSONFileDatabase {
+                file_path: tmpfile
+                    .path()
+                    .to_str()
+                    .expect("failed to convert tmpfile to str")
+                    .to_string(),
+            };
+
+            let story = Story {
+                name: "epic 1".to_owned(),
+                description: "epic 1".to_owned(),
+                status: Status::Open,
+            };
+
+            let epic = Epic {
+                name: "epic 1".to_owned(),
+                description: "epic 1".to_owned(),
+                status: Status::Open,
+                stories: vec![2],
+            };
+
+            let mut stories = HashMap::new();
+            stories.insert(2, story);
+
+            let mut epics = HashMap::new();
+            epics.insert(1, epic);
+
+            let state = DBState {
+                last_item_id: 2,
+                epics,
+                stories,
+            };
+
+            let write_result = db.write_db(&state);
+            let read_result = db.read_db().unwrap();
+
+            assert_eq!(write_result.is_ok(), true);
+
+            assert_eq!(read_result, state);
+        }
+    }
+}
