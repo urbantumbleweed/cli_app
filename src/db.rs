55367c1dd9f3ffebd38bc73b7779239f18a6dffb
Stephen Saunders <saunders.stephen@gmail.com>
2024-09-12 17:13:21 -0700

    Feat: Add JiraDatabase::create_epic
    refactor: Update JiraDatabase to use Box<dyn Database>
    
    - The JiraDatabase struct now uses a dynamic trait object, Box<dyn Database>, instead of a generic type parameter. This allows for more flexibility in choosing the underlying database implementation.
    
    feat: Implement create_epic function in JiraDatabase
    
    - The create_epic function has been implemented in the JiraDatabase struct. It creates a new epic, assigns it a unique ID, and updates the database state accordingly.
    
    feat: Add unit test for create_epic function
    
    - A new unit test, `acreate_a_new_epic`, has been added to test the functionality of the create_epic function in the JiraDatabase struct.
    
    refactor: Update delete function signature in JiraDatabase
    
    - The delete function in the JiraDatabase struct now takes a u32 ID parameter instead of a generic type parameter. This simplifies the function signature and improves clarity.
    
    refactor: Update visibility of MockDB's new function
    
    - The `new` function in the MockDB struct has been changed from private to public. This allows it to be used outside of the module for testing purposes.

--- src/db.rs
+++ src/db.rs
@@ -3,7 +3,7 @@ use std::{fs, io};
 use anyhow::{Context, Error, Result};
 use serde_json::json;
 
-use crate::models::DBState;
+use crate::models::{DBState, Epic};
 
 trait Database {
     fn read_db(&self) -> Result<DBState>;
@@ -27,21 +27,26 @@ impl Database for JSONFileDatabase {
     }
 }
 
-pub struct JiraDatabase<T> {
-    _phantom_data: T,
+pub struct JiraDatabase {
+    database: Box<dyn Database>,
 }
 
-impl<T> JiraDatabase<T> {
-    pub fn create(&mut self, record: T) -> Result<T> {
-        todo!("Implement the ability to create a new Epic and or Story and return the result to the caller")
+impl JiraDatabase {
+    pub fn read_db(&self) -> Result<DBState> {
+        self.database.read_db()
     }
-    pub fn read<U>(&self, id: &U) -> Result<T> {
+    pub fn create_epic(&mut self, epic: Epic) -> Result<u32> {
+        let mut current_state = self.read_db()?;
+        let id = current_state.last_item_id + 1;
+        let _ = &current_state.epics.insert(id, epic.clone());
+        current_state.last_item_id = id;
+        let _ = self.database.write_db(&current_state);
+        Ok(id)
+    }
+    pub fn read<U>(&self, id: &U) -> Result<Epic> {
         todo!("Implement the ability to query the db for an entry based on the hashmap id")
     }
-    pub fn update<V>(&mut self, patch: &V) -> Result<T> {
-        todo!("Implement the ability to update a key with new record. Further development can change this to updating via patch")
-    }
-    pub fn delete<V>(&mut self, id: &V) -> Result<T> {
+    pub fn delete<V>(&mut self, id: u32) -> Result<Epic> {
         todo!("Implement removing database items by id")
     }
 }
@@ -55,7 +60,7 @@ pub mod test_utils {
     }
 
     impl MockDB {
-        fn new() -> Self {
+        pub fn new() -> Self {
             MockDB {
                 last_written_state: RefCell::new(DBState {
                     last_item_id: 0,
@@ -89,11 +94,37 @@ mod tests {
         use std::hash::Hash;
         use std::io::Write;
 
+        use test_utils::MockDB;
+
         use crate::models::{Epic, Status, Story};
 
         use super::JSONFileDatabase;
 
         use super::*;
+
+        #[test]
+        fn create_a_new_epic() {
+            let mut db = JiraDatabase {
+                database: Box::new(MockDB::new()),
+            };
+            let epic = Epic::new("".to_owned(), "".to_owned());
+
+            let result = db.create_epic(epic.clone());
+
+            assert_eq!(result.is_ok(), true, "The result was an error");
+
+            let id = result.unwrap();
+            let db_state = db.read_db().unwrap();
+
+            let expected_id = 1;
+
+            assert_eq!(id, expected_id, "The resulting Id is not incremented");
+            assert_eq!(
+                db_state.last_item_id, expected_id,
+                "The last_item_id should match the incremented id"
+            );
+            assert_eq!(db_state.epics.get(&id), Some(&epic))
+        }
 
         #[test]
         fn read_db_should_fail_with_invalid_path() {
