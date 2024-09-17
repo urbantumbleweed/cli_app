a98712f41190602250405dfca7f6033af73b53c4
Stephen Saunders <saunders.stephen@gmail.com>
2024-09-17 12:43:53 -0700

    feat: add UI module with pages and initial test structure

---
+++ src/ui/pages/mod.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+
+use crate::models::Action;
+
+fn handle_input() -> Result<Option<Action>> {}
+
+fn draw_page() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod handle_input {
+
+        #[test]
+        fn should_fail_() {}
+
+        #[test]
+        fn should_return_none_with_invalid_user_input() {}
+
+        #[test]
+        fn should_produce_an_action_with_valid_user_input() {}
+    }
+
+    mod draw_page {
+
+        #[test]
+        fn should_render_user_actions() {}
+
+        #[test]
+        fn should_render_to_std_out() {}
+    }
+}
