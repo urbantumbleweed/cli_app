a38fe1dfa28dc36dfb5ed5b9dc52200b3bf115b2
Stephen Saunders <saunders.stephen@gmail.com>
2024-09-17 13:52:18 -0700

    test: enhance get_column_string tests for various widths

--- src/ui/pages/page_helpers.rs
+++ src/ui/pages/page_helpers.rs
@@ -13,12 +13,19 @@ mod tests {
         #[test]
         fn should_ellipsify_a_string_to_given_width() {
             let text = "hello world";
-            let width = 5usize;
+            let width0 = 0usize;
+            let width1 = 1usize;
+            let width2 = 2usize;
+            let width3 = 3usize;
+            let width4 = 4usize;
+            let width5 = 5usize;
 
-            let expected = String::from("hello...");
-            let result = get_column_string(&text, width);
-
-            assert_eq!(result, expected);
+            assert_eq!(get_column_string(text, width0), "".to_owned());
+            assert_eq!(get_column_string(text, width1), "h...".to_owned());
+            assert_eq!(get_column_string(text, width2), "he...".to_owned());
+            assert_eq!(get_column_string(text, width3), "hel...".to_owned());
+            assert_eq!(get_column_string(text, width4), "hell...".to_owned());
+            assert_eq!(get_column_string(text, width5), "hello...".to_owned());
         }
 
         #[test]
